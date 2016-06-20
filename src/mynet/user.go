b65f5f441078b76cbdc18c80037002123cb2c454
fudali <fudali133@gmail.com>
2016-06-20 17:08:41 +0800

    test

--- src/mynet/user.go
+++ src/mynet/user.go
@@ -1,11 +1,11 @@
 package mynet
 
 type User struct {
-	Id      int
-	Name    string
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
 	Passwd  string
-	Friends string
-	Other   string
+	Friends string `json:"friends"`
+	Other   string `json:"other"`
 }
 
 func NewUser(id int, name string, pass string, fri string, other string) *User {
